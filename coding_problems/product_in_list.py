8f428d9e95c936932e167ebc641ca756cf9a6374
katelynrm <37051008+katelynrm@users.noreply.github.com>
2019-08-26 18:19:45 -0600

    rename

--- coding_problems/product_in_list.py
+++ coding_problems/product_in_list.py
@@ -1,6 +1,6 @@
 #Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
 
-def add_any_two(list_, k):
+def product_in_list(list_, k):
     products_set = set()
     for i in list_:
         for j in list_[::-1]:
@@ -14,4 +14,4 @@ def add_any_two(list_, k):
 my_list = [10, 15, 3, 7]
 my_num = 17
 
-print(add_any_two(my_list, my_num))
+print(product_in_list(my_list, my_num))
