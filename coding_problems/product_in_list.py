2efd25260f9df7bafabc6fe551f0e9121c9b7b58
katelynrm <katelynrm@hotmail.com>
2019-09-05 20:29:24 -0600

    added hash

--- coding_problems/product_in_list.py
+++ coding_problems/product_in_list.py
@@ -1,5 +1,9 @@
 #Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+my_list = [10, 15, 3, 7]
+my_num = 40
 
+
+#brute force
 def product_in_list(list_, k):
     products_set = set()
     for i in list_:
@@ -11,7 +15,21 @@ def product_in_list(list_, k):
     else:
         return False
 
-my_list = [10, 15, 3, 7]
-my_num = 17
+
+#hash table
+def product_in_list_hash(list_,k):
+    H = dict()
+    for i in range(len(list_)):
+        H[i] = list_[i]
+    num = 0
+    for i in range(len(list_)):
+        y = k - list_[i]
+        if y in H.values():
+            num += 1
+    if num > 0:
+        return True
+    else:
+        return False
 
 print(product_in_list(my_list, my_num))
+print(product_in_list_hash(my_list, my_num))
